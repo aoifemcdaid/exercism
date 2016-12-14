d0872ac07a3eee69a7b3d51ebcd760b53c37937c
Aoife McDaid <aoife.mcdaid@gmail.com>
2016-12-14 21:22:29 +0100

    test 3 ex 2

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,9 +1,13 @@
 class Hamming
   def self.compute(dna1, rna2)
-    if dna1.split('').shift == rna2.split('').shift && dna1.length == rna2.length
-      0
-    else
-      1
+    dna = dna1.split('')
+    rna = rna2.split('')
+    result = 0
+    (0..dna1.length).each do |i|
+      if dna[i] != rna[i]
+        result += 1
+      end
     end
+    result
   end
 end
