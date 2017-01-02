f59ac906882b383b93b129f4d37f4b23132f0224
Aoife McDaid <aoife.mcdaid@gmail.com>
2017-01-02 23:10:44 +0100

    difference of squares

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,24 @@
+require 'active_support/core_ext/enumerable'
+
+
+class Squares
+  def initialize(number)
+    @number = number
+  end
+  def square_of_sum
+    sum = (1..@number).sum
+    return sum**2
+  end
+
+  def sum_of_squares
+    sum = 0
+    (1..@number).each do |value|
+      sum += value**2
+    end
+    return sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
